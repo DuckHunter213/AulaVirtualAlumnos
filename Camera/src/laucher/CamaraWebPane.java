9fd1786992b319bcb25aa81885d8e69a9ae24779
Luis Fernando Gomez Alejandre <gomez.alejandre@gmail.com>
2017-06-07 12:27:03 -0500

    TODO DIJE!

--- Camera/src/laucher/CamaraWebPane.java
+++ Camera/src/laucher/CamaraWebPane.java
@@ -1,6 +1,5 @@
 package laucher;
 
-import com.github.sarxos.webcam.Webcam;
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.net.InetSocketAddress;
@@ -20,34 +19,39 @@ public class CamaraWebPane extends Pane {
 
     int port;
     String host;
-    Webcam webcam;
     static VideoPanel panel = new VideoPanel();
-    private final static Dimension dimension = new Dimension(320, 240);
+    private Pane panelVideo;
+    private final static Dimension dimension = new Dimension(640,480);
     protected final static Logger logger = LoggerFactory.getLogger(StreamClient.class);
 
     public CamaraWebPane(String host,int port) {
-        final SwingNode swingNode = new SwingNode();
-        createAndSetSwingContent(swingNode);
+        panelVideo = new Pane();
         this.host = host;
         this.port = port;
+        final SwingNode swingNode = new SwingNode();
+        createAndSetSwingContent(swingNode);
+        
+        panelVideo.getChildren().add(swingNode);
     }    
     
+    public Pane getPanelVideo(){
+        return this.panelVideo;
+    }
     private void createAndSetSwingContent(final SwingNode swingNode) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 JPanel panel2 = new JPanel();
                 panel2.setSize(dimension);
-                panel2.setSize(320, 240);
                 panel2.setPreferredSize(dimension);
 
-                logger.info("setup dimension :{}", dimension);
-                StreamClientAgent clientAgent = new StreamClientAgent(new StreamFrameListenerIMPL(), dimension);
+                logger.info("setup dimension :{}",dimension);
+                StreamClientAgent clientAgent = new StreamClientAgent(new SwingNodeExample.StreamFrameListenerIMPL(), dimension);
                 clientAgent.connect(new InetSocketAddress(host, port));
-
-                panel.setSize(320, 240);
+                
+                panel.setSize(dimension);
                 panel.setPreferredSize(dimension);
-
+                
                 panel2.add(panel);
                 swingNode.setContent(panel2);
             }
--- Camera/src/laucher/SwingNodeExample.java
+++ Camera/src/laucher/SwingNodeExample.java
@@ -1,12 +1,10 @@
 package laucher;
 
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.net.InetSocketAddress;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.embed.swing.SwingNode;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -17,27 +15,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import us.sosia.video.stream.agent.StreamClient;
 import us.sosia.video.stream.agent.StreamClientAgent;
-import us.sosia.video.stream.agent.StreamServerAgent;
 import us.sosia.video.stream.agent.ui.VideoPanel;
 import us.sosia.video.stream.handler.StreamFrameListener;
+import laucher.Servidor;
 
 public class SwingNodeExample extends Application {
-
-    Webcam webcam;
+    
     static VideoPanel panel = new VideoPanel();
-    private final static Dimension dimension = new Dimension(320, 240);
+    private final static Dimension dimension = new Dimension(400, 462);
     protected final static Logger logger = LoggerFactory.getLogger(StreamClient.class);
 
     @Override
     public void start(Stage stage) {
+        Servidor servidor = new Servidor("localhost", 20000);
         final SwingNode swingNode = new SwingNode();
         createAndSetSwingContent(swingNode);
 
         Pane pane = new Pane();
         pane.getChildren().add(swingNode); // Adding swing node
 
-        CamaraWebPane mejoras = new CamaraWebPane("localhost",20000);
-        stage.setScene(new Scene(mejoras, 320, 240));
+        CamaraWebPane camaraWebPane = new CamaraWebPane("localhost", 20000);
+        stage.setScene(new Scene(camaraWebPane.getPanelVideo(), 400, 462));
         stage.show();
     }
 
@@ -47,16 +45,17 @@ public class SwingNodeExample extends Application {
             public void run() {
                 JPanel panel2 = new JPanel();
                 panel2.setSize(dimension);
-                panel2.setSize(320, 240);
                 panel2.setPreferredSize(dimension);
 
                 logger.info("setup dimension :{}",dimension);
                 StreamClientAgent clientAgent = new StreamClientAgent(new StreamFrameListenerIMPL(), dimension);
                 clientAgent.connect(new InetSocketAddress("localhost", 20000));
                 
-                panel.setSize(320,240);
+                panel.setSize(dimension);
                 panel.setPreferredSize(dimension);
+                
 
+                
                 panel2.add(panel);
                 swingNode.setContent(panel2);
 
